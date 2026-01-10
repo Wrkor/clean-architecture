ac047e2f65af76c4806aa6c418d0535b5ceee478
wrkor <sashapotapov31@yandex.ru>
2026-01-10 18:51:40 +0800

    add abstractions, domain exceptions, value objects

---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,8 @@
+namespace Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity { }
---
+++ Domain/ValueObjects/TopicId.cs
@@ -0,0 +1,19 @@
+namespace Domain.ValueObjects;
+
+public record TopicId
+{
+    public Guid Value { get; }
+
+    private TopicId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static TopicId Of(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new DomainException($"{nameof(TopicId)} не может быть пустым");
+
+        return new TopicId(value);
+    }
+}
