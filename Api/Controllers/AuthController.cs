7967b9f840c0c9cfefe7d9ec0168ff8d77939f2e
wrkor <sashapotapov31@yandex.ru>
2026-01-11 16:38:18 +0800

    added command register

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Controllers;
 
 [AllowAnonymous]
@@ -9,7 +11,7 @@ public class AuthController(
 ) : ControllerBase
 {
     [HttpPost("login")]
-    public async Task<IResult> Login(LoginRequestDto @object)
+    public async Task<IResult> LoginAsync(LoginRequestDto @object)
     {
         var user = await manager.FindByEmailAsync(@object.Email);
         if (user is null)
@@ -26,6 +28,46 @@ public class AuthController(
             user.Email!,
             token
         );
+        return Results.Ok(new { @object = result });
+    }
+
+    [HttpPost("register")]
+    public async Task<IResult> RegisterAsync(RegisterUserRequestDto @object)
+    {
+        var isUserExists = await manager.Users.AnyAsync(u =>
+            u.UserName == @object.UserName
+        );
+
+        if (isUserExists)
+            return Results.BadRequest("Username занят");
+
+        isUserExists = await manager.Users.AnyAsync(u =>
+            u.Email == @object.Email
+        );
+
+        if (isUserExists)
+            return Results.BadRequest("Email занят");
+
+        var user = new CustomIdentityUser()
+        {
+            FullName = @object.FullName,
+            Email = @object.Email,
+            UserName = @object.UserName,
+            About = string.Empty,
+        };
+
+        var identityResult = await manager.CreateAsync(user, @object.Password);
+
+        if (!identityResult.Succeeded)
+            return Results.BadRequest(identityResult.Errors);
+
+        var token = jwtService.CreateToken(user);
+        var result = new IdentityUserResponseDto(
+            user.UserName,
+            user.Email,
+            token
+        );
+
         return Results.Ok(new { @object = result });
     }
 }
